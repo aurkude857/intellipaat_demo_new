336d536b1c0ad74f2344faa448f42c24ba80e567
Rabi <Rabi0123br@gmail.com>
2022-07-17 17:01:34 +0000

    developer-2 changes - merging changes

--- python.py
+++ python.py
@@ -2,3 +2,10 @@ numbers = [1,2,3,4,5]
 
 for num in numbers:
   print (num)
+
+items = ["one","tw","three","four"]
+
+for item in items:
+  if item == "five":
+    print ("Found item")
+  print (item)
